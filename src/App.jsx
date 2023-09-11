8e8504e3d1ccc66010928db719c1f4858e94a245
Miguelsanchez.dev <info@miguelsanchez.dev>
2023-09-11 08:02:16 +0200

    no muestra contenido square

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,134 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import { useState } from 'react';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const TURNS = {
+  X: "x",
+  O: "o",
+}
+
+
+
+const Square = ({ children, isSelected, updateBoard, index }) => {
+
+  
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+
+  const handleClick = () => {
+    updateBoard()
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  )
+}
+
+const WINNER_COMBOS = [
+  [0, 1, 2], //horizontal
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6], //vertical
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8], //diagonal
+  [2, 4, 6]
+]
+
+function App() {
+
+  const [board, setBoard] = useState(Array(9).fill(null))
+
+  const [turn, setTurn] = useState(TURNS.X)
+
+  const [winner, setWinner] = useState(null) //null, no hay ganador, false = empate
+
+  const checkWinner = (boardToCheck) => {
+    //revisamos las combinaciones ganadoras
+    for (const combo of WINNER_COMBOS){
+      const [a, b, c] = combo
+      if(boardToCheck[a] && boardToCheck[a] === boardToCheck[b] && boardToCheck[a] === boardToCheck[c]){
+        return boardToCheck[a]
+      }
+    }
+    //si no hay ganador
+    return null
+  }
+
+  const resetGame = () => {
+    
+  }
+
+  const updateBoard = (index) => {
+    //no actualizamos si ya esta escrita
+    if(board[index] || winner) return
+    const newBoard = [ ...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+    //comprobamos que hay ganador
+    const newWinner = checkWinner(newBoard)
+    if(newWinner){
+      setWinner(newWinner)
+    }
+  }
+
+  return (
+    <main className="board">
+      <h1>Tic Tac Toe</h1>
+      <section className="game">
+        {
+          board.map((_, index) => {
+            return (
+              <Square
+                key={index}
+                index={index}
+                updateBoard={updateBoard}
+                >
+                  {board[index]}
+                </Square>
+            )
+          })
+        }
+      </section>
+
+      <section className="turn">
+        <Square isSelected = {turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected = { turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
+
+      {
+        winner !== null && (
+          <section className="winner">
+            <div className="text">
+              <h2>
+                {
+                  winner === false ? "Empate" : "Gano" + winner
+                }
+              </h2>
+
+              <header className="win">
+                {winner && <Square>{winner}</Square>}
+              </header>
+
+              <footer>
+                <button onClick={resetGame}>
+                  Empezar de nuevo
+                </button>
+              </footer>
+            </div>
+          </section>
+        )
+      }
+    </main>
   )
 }
 
